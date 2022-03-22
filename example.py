fc933d34de35791e830fef234994eb543000e185
Gergo Schreiner <gergo.schreiner@logmein.com>
2022-03-22 21:56:57 +0100

    init commit

--- example.py
+++ example.py
@@ -1,13 +1,6 @@
-# GUIWizard
-Defines a tkinter window and frames/panels to make the GUI development more transparent and easier.      Think in panels and design your application according to them. Tear down your GUI to panels and put the widgets on them.
+from guiwizard import *
 
-## Defining the panels
 
-### Left panel
-
-Each panel can be described by a function:
-
-```
 def left_panel(panel: Frame, radio_foodtime_type: StringVar) -> Tuple[List[Widget], dict]:
     widgets: List[Widget] = []
     widgets.append(Label(panel, text='When do you eat?', name='eatlabel'))
@@ -22,31 +15,8 @@ def left_panel(panel: Frame, radio_foodtime_type: StringVar) -> Tuple[List[Widge
                       }
 
     return widgets, packing_params
-```
 
-We need to provide a parent component, which is a panel or `Frame` and other variables to the GUI as parameters. Then we start adding `Widgets` to a list and defining the belonging parameters.
 
-We have to refer to the components with their names.
-
-Give a name to a component:
-
-`widgets.append(Label(panel, text='When do you eat?', name='eatlabel'))`
-
-Link it with parameters:
-
-`{'eatlabel': {'pady': (0, 20)}...`
-
-If you do not name the components, you can still refer to them with the following names for example:
-
-`radiobutton`
-
-`radiobutton1`
-
-`...`
-
-### Right panel
-
-```
 def right_panel(panel: Frame) -> Tuple[List[Widget], dict]:
     widgets: List[Widget] = []
     widgets.append(Label(panel, text='What is you favorite food?'))
@@ -54,30 +24,16 @@ def right_panel(panel: Frame) -> Tuple[List[Widget], dict]:
     widgets.append(Button(panel, text="Let's eat", command=get_my_fav_food))
     packing_params = {'fav_food': {'pady': (20, 90)}}
     return widgets, packing_params
-```
 
-After we defined the panels we can add more logic to our application:
 
-```
 def get_my_fav_food():
     foodtime = radio_foodtime_type.get()
     fav_food = gui.get_component_from_window('fav_food').get()
     result_entry = gui.get_component(0, 'result')
     result_entry.insert(0, f'I usually eat {fav_food} in {foodtime}.')
     gui.update_ui({'result': {'state': 'readonly'}})
-```
 
-We can get a component by calling GUIWizard methods:
- - get_component_from_window()
- - get_component()
 
-This system is more practical when you have a massive amount of Widgets on your window and you use the grid packing system. It is easy to lost in the rows and columns, plus you need to calculate and align them. 
-
-With this technique you organise your `Widgets` by panels and names. Tearing down a complex GUI makes the whole picture transparent and development could be easier. 
-
-## Creating the window, panels and put them together
-
-```
 gui = GUIWizard('My Exmaple GUI App', 1000, 760, 20, 20, 500, 450, 'logo.png')
 gui.create_panels(2)
 
@@ -92,28 +48,3 @@ gui.pack_on_panel(left_widgets, left_widget_params)
 gui.pack_on_panel(right_widgets, right_widget_params)
 gui.pack_on_window()
 gui.window.mainloop()
-```
-
-## Try it out
-
-`python3 example.py`
-
-The created GUI with GUIWizard:
-
-![GUI](https://raw.githubusercontent.com/needsomesl33p//
-guiwizard/master/images/init.png)
-
-After some interaction:
-
-![Wizard](https://raw.githubusercontent.com/needsomesl33p//
-guiwizard/master/images/after.png)
-
-### Tested on
-
-`Python 3.9.10`
-
-### Requirements
-
-`tkinter`
-
-
---
+++ guiwizard.py
@@ -0,0 +1,80 @@
+from tkinter import *
+from typing import List, Tuple
+
+
+class GUIWizard(object):
+    '''Defines a tkinter window and frames/panels to make the GUI development more transparent and easier. 
+    Think in panels and design your application according to them. Tear down your GUI to panels and put the widgets on.'''
+
+    def __init__(self, window_title: str, window_width: int, window_height: int, padx: int, pady: int,
+                 canvas_width=None, canvas_height=None, image_path=None) -> None:
+        self.window = Tk()
+        self.window.geometry(f'{window_width}x{window_height}')
+        self.window.title(window_title)
+        self.window.config(padx=padx, pady=pady)
+        if image_path:
+            self.canvas = Canvas(width=canvas_width,
+                                 height=canvas_height, highlightthickness=0)
+            self.image = PhotoImage(file=image_path)
+            self.canvas.create_image(
+                canvas_width/2, canvas_height/2, image=self.image)
+            self.canvas.pack()
+
+    @property
+    def panels(self) -> List[Frame]:
+        return self._panels
+
+    @panels.setter
+    def panels(self, value: List[Frame]) -> None:
+        self._panels = value
+
+    def get_component(self, panel_number: int, component_name: str) -> Widget:
+        '''Returns a Widget component. You need to provide the number of the panel where the specific component can be located.'''
+        panel = self._panels[panel_number]
+        components = panel.winfo_children()
+        for item in components:
+            if f'.panel{panel_number}.{component_name}' == str(item):
+                return item
+
+    def get_component_from_window(self, componenet_name: str) -> Widget:
+        '''Returns a Widget componenet from the window.'''
+        panels_len = len(self._panels)
+        for index in range(panels_len):
+            item = self.get_component(index, componenet_name)
+            if item:
+                return item
+
+    def update_ui(self, components: dict) -> None:
+        '''Updates or configures the UI components with the specified parameters.'''
+        for component_name, configs in components.items():
+            component: Widget = self.get_component_from_window(component_name)
+            component.configure(**configs)
+
+    def create_panels(self, number: int) -> None:
+        '''Create as many panels or frames as the number parameter gets.'''
+        frames: List[Frame] = []
+        for i in range(number):
+            frame = Frame(self.window, name=f'panel{i}')
+            frames.append(frame)
+        self._panels = frames
+
+    def pack_on_panel(self, wigets: List[Widget], packing_params: dict) -> None:
+        '''Packs every Widget in the list on on the panel. By the tkinter design, you need to provide the parent of the child wiget, which is the panel.'''
+        for item in wigets:
+            branch_name = str(item)
+            split_chr = '!' if '!' in branch_name else '.'
+            name = branch_name.split(split_chr)[-1]
+            if name in packing_params.keys():
+                item.pack(**packing_params[name])
+            else:
+                item.pack()
+
+    def pack_on_panel_same_config(self, wigets: List[Widget], kwargs: dict) -> None:
+        '''Every Widget item will be configured with the same arguments or parameters.'''
+        for item in wigets:
+            item.pack(**kwargs)
+
+    def pack_on_window(self) -> None:
+        '''Packs the panels on the window. It starts the packing from a left and goes to the right.'''
+        for frame in self._panels:
+            frame.pack(side='left', expand=True)
